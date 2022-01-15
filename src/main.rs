3271a986e64f3a07b76f8db0e3ddf98ea2a4f4ae
Ansh Anurag <anxhnurag@gmail.com>
2022-01-15 14:07:06 +0530

    more learning

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,13 @@
 // }
 
 fn main() {
-    let num = 1;
-    if num==1 {
-        println("ttt");
-    }else if n==2 {
-        println("ggg");
-    }else{
-        println("ll");
+    let mut num = 1;
+    loop{
+        x*=2;
+        if x > 5000 {
+            break;
+        }
+        println!("The val of x is now is {}", x);
     }
 }
 
