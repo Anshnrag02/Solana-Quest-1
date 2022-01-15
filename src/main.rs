8186ce1aa3972631b9b3724b1668d7d2a791f397
Ansh Anurag <anxhnurag@gmail.com>
2022-01-15 13:38:26 +0530

    more learning

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,10 @@
-fn main() {
-    let x = "Hello";
-    println!("{}, world!",x);
+//
+fn print_type_of<T>(_: &T) {
+    println!("{}", std::any::type_name::<T>())
 }
+
+fn main() {
+    let x = (5,"pokemon",false);// mut is reqd. otherwise variables are immutable
+    println!("{}, world! {}",x.1,x.2);
+}
+
